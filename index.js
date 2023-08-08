64d72c9a637643198ca224ddfd194f9ab317a57c
Gui7001br <113148079+Gui7001br@users.noreply.github.com>
2023-08-08 15:15:38 -0400

    Criação do UPDATE de dados

--- index.js
+++ index.js
@@ -32,12 +32,26 @@ bd.connect((err) => {
  });
 
 // Inserir os dados os dados
+
 bd.query("INSERT INTO usuario (id, nome, email) values (3, 'José', 'jose@gmail.com')",
     (err, result) => {
         if(err) {
             console.log("Erro de inserção");
         } else {
             console.log("Inserção realizada com sucesso");
+        }
+    }
+)
+
+// Atualizar os dados do banco de dados
+
+bd.query("UPDATE usuario SET nome = 'José da Silva' where id = 3",
+    (err, result) => {
+        if(err) {
+            console.log("Erro de atualização");
+        } else {
+            console.log("Atualização realizada com sucesso");;
+            console.log(result.rows);
         }
     }
 )
